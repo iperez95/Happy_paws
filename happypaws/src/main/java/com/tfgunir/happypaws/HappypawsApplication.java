6cad368f213915bed5cd76d741e2aba04450e7f0
Davasda <menendez.david@gmail.com>
2023-10-07 11:32:45 +0200

    Creación del código.
    Para que arranque tiene que conectar a una bbdd. Eso se configura en el archivo application.properties y lo modificaremos cuando esté creada la base de datos.

---
+++ happypaws/src/main/java/com/tfgunir/happypaws/HappypawsApplication.java
@@ -0,0 +1,13 @@
+package com.tfgunir.happypaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappypawsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappypawsApplication.class, args);
+	}
+
+}
---
+++ happypaws/src/test/java/com/tfgunir/happypaws/HappypawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfgunir.happypaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappypawsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
