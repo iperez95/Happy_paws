fde6cb18b605fc284d53716284b0a1cf4ed86f2b
Davasda <menendez.david@gmail.com>
2023-10-22 20:21:11 +0200

    Añadido Query protectoras por provincias
    Faltan crear los métodos que lo implementan.

--- backend/src/main/java/com/tfgunir/happypaws/modelo/repository/ProtectoraRepository.java
+++ backend/src/main/java/com/tfgunir/happypaws/modelo/repository/ProtectoraRepository.java
@@ -1,11 +1,27 @@
 package com.tfgunir.happypaws.modelo.repository;
 
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.tfgunir.happypaws.modelo.entities.Protectora;
 
 
 public interface ProtectoraRepository extends JpaRepository<Protectora, Integer> {
        
+@Query(value = "SELECT prote.*, muni.MUNICIPIO, prov.PROVINCIA FROM protectoras prote\r\n"
+            + "inner join municipios muni on muni.IDMUNICIPIO = prote.IDMUNICIPIO \r\n"
+            + "inner join provincias prov on prov.IDPROVINCIA = muni.IDPROVINCIA \r\n"
+            + "where prov.PROVINCIA = ?1",
+			nativeQuery = true)    
+List<Protectora> protetorasPorNombreProvincia(String nombre);
 
+@Query(value = "SELECT prote.*, muni.MUNICIPIO, prov.PROVINCIA FROM protectoras prote\r\n"
+            + "inner join municipios muni on muni.IDMUNICIPIO = prote.IDMUNICIPIO \r\n"
+            + "inner join provincias prov on prov.IDPROVINCIA = muni.IDPROVINCIA \r\n"
+            + "where prov.IDPROVINCIA = ?1",
+			nativeQuery = true)    
+List<Protectora> protetorasPorIdProvincia(int idprovincia);
 }
