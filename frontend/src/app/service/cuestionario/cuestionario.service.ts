027566d9f55d8975d6e1475e095c43dea733ce9e
Davasda <menendez.david@gmail.com>
2023-11-13 22:45:22 +0100

    creado doc cuestionario.service.ts

--- frontend/src/app/service/cuestionario/cuestionario.service.ts
+++ frontend/src/app/service/cuestionario/cuestionario.service.ts
@@ -8,48 +8,80 @@ import { AxiosService } from "../axios/axios.service";
 
 
 @Injectable({
-    providedIn: 'root'
-  })
+  providedIn: 'root'
+})
 
-  
-  export class CuestionarioService{
 
-    //URL del servicio Rest
+/**
+ * Servicio para manejar las operaciones relacionadas con el cuestionario.
+ */
+export class CuestionarioService {
+
+  //URL del servicio Rest
   readonly endpoint = axios.defaults.baseURL;
-  
+
+
   /**
-   * Encargado de hacer las peticiones HTTP a nuestro servicio REST
-   * @param _httpClient 
+   * Crea una instancia de CuestionarioService.
+   * @param {HttpClient} _httpClient - La instancia de HttpClient inyectada.
+   * @param {AxiosService} axiosService - La instancia de AxiosService inyectada.
+   * @memberof CuestionarioService
    */
-  constructor(private _httpClient : HttpClient, private axiosService: AxiosService) { 
+  constructor(private _httpClient: HttpClient, private axiosService: AxiosService) {
   }
- 
-  
 
-  obtenerPreguntas(): Observable<any>{
+
+  /**
+   * Obtiene la lista de preguntas para el cuestionario.
+   * @returns Un Observable que emite la lista de preguntas.
+   */
+  obtenerPreguntas(): Observable<any> {
     return this._httpClient.get(`${this.endpoint}/cuestionario/preguntas`)
-    .pipe(catchError(this.manejarError));
+      .pipe(catchError(this.manejarError));
   }
 
-// ENVIA RESUPUESTAS FORMULARIO CON TOKEN SESION
- enviarRespuestas(respuestas: RespuestasAdoptante[]): Observable<Object> {
-  const token = this.axiosService.getAuthToken();
-  if (!token) {
-    throw new Error('No hay token');
-  }
-  const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  // ENVIA RESUPUESTAS FORMULARIO CON TOKEN SESION
+  /**
+   * Envía las respuestas dadas al servidor para su almacenamiento.
+   * @param respuestas - Las respuestas a enviar.
+   * @returns Un Observable que resuelve a la respuesta del servidor.
+   * @throws Un error si no hay un token de autenticación.
+   */
+  enviarRespuestas(respuestas: RespuestasAdoptante[]): Observable<Object> {
+    const token = this.axiosService.getAuthToken();
+    if (!token) {
+      throw new Error('No hay token');
+    }
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     console.log('Token:', token);
     console.log('Headers:', headers);
-  return this._httpClient.post(`${this.endpoint}/cuestionario/guardar`, respuestas, {headers})
-    .pipe(catchError(this.manejarError));;
+    return this._httpClient.post(`${this.endpoint}/cuestionario/guardar`, respuestas, { headers })
+      .pipe(catchError(this.manejarError));;
   }
 
-
-  
+  /**
+   * Método que maneja los posibles errores de las llamadas al servicio rest
+   * @param error 
+   * @returns un objeto de tipo Observable que contendrá el error que ha ocurrido
+   */
+  private manejarError(e: HttpErrorResponse) {
+    let mensajeError = ''
+    //Diferenciamos si el error es del servidor o más genérico
+    if (e.error instanceof ErrorEvent) {
+      mensajeError = 'Ha ocurrido un error:' + e.error
+    } else {
+      mensajeError = `El servicio Rest retorno: Status: ${e.status}, ` +
+        `Body: ${e.error}`
+    }
+    //Imprimimos el mensaje de error y lo arrojamos médiante una función lambda
+    //Esta manerá tenemos que hacerla así cuando trabajamos con Observables.
+    console.error(mensajeError)
+    return throwError(() => new Error(mensajeError));
+  }
 
   //ORIGINAL FUNCIONANDO SIN USUARIO EN SESIÓN
   // enviarRespuestas(respuestas: RespuestasAdoptante[]): Observable<any> {
-    
+
   //   return this._httpClient.post<any>(`${this.endpoint}/cuestionario/guardar`, respuestas);
   // }
 
@@ -60,27 +92,5 @@ import { AxiosService } from "../axios/axios.service";
   //   .pipe(catchError(this.manejarError));
   // }
 
-
-
-
-  /**
-   * Método que maneja los posibles errores de las llamadas al servicio rest
-   * @param error 
-   * @returns un objeto de tipo Observable que contendrá el error que ha ocurrido
-   */
-  private manejarError(e: HttpErrorResponse){
-    let mensajeError = ''
-    //Diferenciamos si el error es del servidor o más genérico
-    if (e.error instanceof ErrorEvent) {
-      mensajeError = 'Ha ocurrido un error:' + e.error
-    } else {
-      mensajeError = `El servicio Rest retorno: Status: ${e.status}, ` +
-            `Body: ${e.error}`
-    }
-    //Imprimimos el mensaje de error y lo arrojamos médiante una función lambda
-    //Esta manerá tenemos que hacerla así cuando trabajamos con Observables.
-    console.error(mensajeError)
-    return throwError(() => new Error(mensajeError));
-  }
 
 }
