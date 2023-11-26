6e5cf188ee11cffd845ced0f7a2d8467659deb81
raulmoreno <raulmro@hotmail.es>
2023-11-26 19:41:31 +0100

    gestion animal falta id protectora

--- frontend/src/app/components/animales/gestion/gestion-animal/gestion-animal.component.ts
+++ frontend/src/app/components/animales/gestion/gestion-animal/gestion-animal.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { Animal } from 'src/app/entidades/animal';
+import { Protectora } from 'src/app/entidades/protectora';
+import { AnimalService } from 'src/app/service/animal/animal.service';
+import { ProtectoraService } from 'src/app/service/protectora/protectora.service';
 
 @Component({
   selector: 'app-gestion-animal',
@@ -7,4 +12,51 @@ import { Component } from '@angular/core';
 })
 export class GestionAnimalComponent {
 
+  @Input() protectora: Protectora;
+  @Input() animales: Animal[];
+  public listaAnimales : Animal[] = []
+  idprotectora : number = 1;
+
+  constructor(private _animalService : AnimalService, private router :Router, private _protrectoraService: ProtectoraService) { 
+  }
+
+  ngOnInit():void {
+    this.listar();
+  }
+
+  public listar(){
+    this._animalService.listarAnimales().subscribe(dato => {
+      this.listaAnimales = dato;
+      console.log(this.listaAnimales);
+    });
+ }
+
+  // public listar(){
+  //   this._animalService.listarAnimalPorIdProtectora(this.protectora.idprotectora).subscribe(data => {
+  //     this.listaAnimales = data;
+  //     console.log(this.listaAnimales);
+  //   });
+  // }
+  public irAltaAnimal() {
+    this.router.navigate(['/animales/gestion/alta']);
+   }
+
+  //  private animalesPorProtectora() {
+  //   this.listaAnimales = this.animales.filter(animal => animal.protectora.idprotectora === this.protectora.idprotectora);
+  // }
+
+  public irModificalAnimal(idanimal: number) {
+    this.router.navigate(['/animales/gestion/modificar', idanimal]);
+   }
+
+   public BorrarAnimal(idanimal: number){
+    this._animalService.eliminarAnimal(idanimal).subscribe(() => {
+      this.router.navigate(['/animales/gestion']);
+    },
+    (error) => {
+      console.error("Error al eliminar el animal:", error);
+    });
+    
+
+   }
 }
