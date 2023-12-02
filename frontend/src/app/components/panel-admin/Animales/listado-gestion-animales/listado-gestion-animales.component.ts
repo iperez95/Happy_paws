49ab4fbb8b10e4453757a7623c88edfd393332ac
Davasda <menendez.david@gmail.com>
2023-12-02 09:20:32 +0100

    añaido service e iniciado listado

--- frontend/src/app/components/panel-admin/Animales/listado-gestion-animales/listado-gestion-animales.component.ts
+++ frontend/src/app/components/panel-admin/Animales/listado-gestion-animales/listado-gestion-animales.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Animal } from 'src/app/entidades/animal';
+import { AnimalAdminService } from 'src/app/service/panel-admin/animal-admin.service';
 
 @Component({
   selector: 'app-listado-gestion-animales',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./listado-gestion-animales.component.css']
 })
 export class ListadoGestionAnimalesComponent {
+
+  listaAnimales: Animal[]=[];
+
+  constructor(private _animalAdminService:AnimalAdminService, private router:Router) { }
+
+  ngOnInit():void{
+    this.listar();
+
+  }
+
+  public listar(){
+    this._protectoraAdminService.listaProtectoras().subscribe(dato => { 
+      this.listaProtectoras = dato;
+      console.log(this.listaProtectoras);
+    });
+  }
 
 }
